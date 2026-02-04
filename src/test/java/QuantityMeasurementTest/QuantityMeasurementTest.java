6cf6c92a58e57548f6e3c7f4bc018693cf99058d
Nilesh Patil <nileshpatil2854@gmail.com>
2026-02-03 21:48:55 -0800

    add: feat equality program

---
+++ src/test/java/QuantityMeasurementTest/QuantityMeasurementTest.java
@@ -0,0 +1,36 @@
+package QuantityMeasurementTest;
+import org.example.QuantityMeasurement;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.example.QuantityMeasurement.CompareValue;
+
+public class QuantityMeasurementTest {
+
+    @Test
+    public void testFeetEquality_SameValue(){
+        CompareValue f1=new CompareValue(1.0);
+        CompareValue f2=new CompareValue(1.0);
+        assertTrue(f1.equals(f2));
+    }
+
+    @Test
+    public void testFeetEquality_DifferentValue(){
+        CompareValue f1=new CompareValue(1.0);
+        CompareValue f2=new CompareValue(2.0);
+        assertFalse(f1.equals(f2));
+    }
+
+    @Test
+    public void testFeetEquality_NullComparison(){
+        CompareValue f1=new CompareValue(1.0);
+        assertFalse(f1.equals(null));
+    }
+
+    @Test
+    public void testFeetEquality_SameReferance(){
+        CompareValue f1=new CompareValue(1.0);
+        assertTrue(f1.equals(f1));
+    }
+}
