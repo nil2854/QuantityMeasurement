5fb81ac1e21c72c85178276bb7ec07ca7cdfa7a7
Nilesh Patil <nileshpatil2854@gmail.com>
2026-02-04 02:03:51 -0800

    add: feat inches measurement

--- src/main/java/org/example/QuantityMeasurement.java
+++ src/main/java/org/example/QuantityMeasurement.java
@@ -1,37 +1,80 @@
 package org.example;
-
 import java.util.Scanner;
 
 public class QuantityMeasurement {
+    public static class FeetCompare {
+        private final double value;
 
-    public static class CompareValue {
-        public double value;
-        public CompareValue(double value) {
+        //compare constructor for feet
+        public FeetCompare(double value) {
             this.value = value;
         }
 
-        public boolean equals(Object val) {
-            if (this == val) {
+        public boolean equals(Object obj) {
+            if (this == obj)
                 return true;
-            }
-            if (val == null) {
+            if (obj == null || getClass() != obj.getClass())
                 return false;
-            }
-            CompareValue other = (CompareValue) val;
-            return Double.compare(this.value, other.value) == 0;
+            FeetCompare f = (FeetCompare) obj;
+            return Double.compare(this.value, f.value) == 0;
         }
     }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter 1st value:");
-        double value1 = scanner.nextDouble();
-        System.out.print("Enter 2nd value:");
-        double value2 = scanner.nextDouble();
-        CompareValue opt1 = new CompareValue(value1);
-        CompareValue opt2 = new CompareValue(value2);
 
-        boolean result = opt1.equals(opt2);
-        System.out.println("Value are equal -- " + result);
-        scanner.close();
+    public static class InchesCompare {
+        private final double value;
+
+        //compare constructor for inches
+        public InchesCompare(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+
+            InchesCompare other = (InchesCompare) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+
     }
+
+    static boolean CheckFeetEquality (double v1, double v2){
+        FeetCompare f1 = new FeetCompare(v1);
+        FeetCompare f2 = new FeetCompare(v2);
+        return f1.equals(f2);
+    }
+
+    static boolean CheckInchesEquality(double v1, double v2){
+        InchesCompare i1 = new InchesCompare(v1);
+        InchesCompare i2 = new InchesCompare(v2);
+        return i1.equals(i2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Quantity Measurement Application");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first feet value:");
+        double ft1 = sc.nextDouble();
+
+        System.out.println("Enter second feet value:");
+        double ft2 = sc.nextDouble();
+
+        boolean feetResult = CheckFeetEquality(ft1, ft2);
+        System.out.println("Feet Equality Result: " + feetResult);
+
+        System.out.println("Enter first inches value:");
+        double inc1 = sc.nextDouble();
+
+        System.out.println("Enter second inches value:");
+        double inc2 = sc.nextDouble();
+
+        boolean inchesResult = CheckInchesEquality(inc1, inc2);
+        System.out.println("Inches Equality Result: " + inchesResult);
+    }
+
 }
--- src/test/java/QuantityMeasurementTest/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest/QuantityMeasurementTest.java
@@ -1,36 +1,77 @@
 package QuantityMeasurementTest;
-import org.example.QuantityMeasurement;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.example.QuantityMeasurement.CompareValue;
+import org.example.QuantityMeasurement.FeetCompare;
+import org.example.QuantityMeasurement.InchesCompare;
 
 public class QuantityMeasurementTest {
 
     @Test
     public void testFeetEquality_SameValue(){
-        CompareValue f1=new CompareValue(1.0);
-        CompareValue f2=new CompareValue(1.0);
+        FeetCompare f1=new FeetCompare(1.0);
+        FeetCompare f2=new FeetCompare(1.0);
         assertTrue(f1.equals(f2));
     }
 
     @Test
     public void testFeetEquality_DifferentValue(){
-        CompareValue f1=new CompareValue(1.0);
-        CompareValue f2=new CompareValue(2.0);
+        FeetCompare f1=new FeetCompare(1.0);
+        FeetCompare f2=new FeetCompare(2.0);
         assertFalse(f1.equals(f2));
     }
 
     @Test
     public void testFeetEquality_NullComparison(){
-        CompareValue f1=new CompareValue(1.0);
+        FeetCompare f1=new FeetCompare(1.0);
         assertFalse(f1.equals(null));
     }
 
     @Test
+    public void testFeetEquality_NonNumericInput(){
+        FeetCompare f1=new FeetCompare(1.0);
+        assertFalse(f1.equals("one"));
+    }
+
+    @Test
     public void testFeetEquality_SameReferance(){
-        CompareValue f1=new CompareValue(1.0);
+        FeetCompare f1=new FeetCompare(1.0);
         assertTrue(f1.equals(f1));
     }
+    //for inches
+    @Test
+    public void testInchesEquality_SameValue(){
+        InchesCompare i1=new InchesCompare(1.0);
+        InchesCompare i2=new InchesCompare(1.0);
+        assertTrue(i1.equals(i2));
+    }
+
+    @Test
+    public void testInchesEquality_DifferentValue(){
+        InchesCompare i1=new InchesCompare(1.0);
+        InchesCompare i2=new InchesCompare(2.0);
+        assertFalse(i1.equals(i2));
+    }
+
+    @Test
+    public void testInchesEquality_NullComparison(){
+        InchesCompare i1=new InchesCompare(1.0);
+        assertFalse(i1.equals(null));
+    }
+
+    @Test
+    public void testInchesEquality_NonNumericInput(){
+        InchesCompare i1=new InchesCompare(1.0);
+        assertFalse(i1.equals("one"));
+    }
+
+    @Test
+    public void testInchesEquality_SameReferance(){
+        InchesCompare i1=new InchesCompare(1.0);
+        assertTrue(i1.equals(i1));
+    }
+
 }
+
